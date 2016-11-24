380f116ef101ff406c960cdf033a6060f1522a93
dietskan <dietskan@dietskans-macbook-pro.local>
2016-11-24 09:41:22 -0800

    Initial Commit

---
+++ Target Support Files/Pods-Project2UITests/Pods-Project2UITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Project2UITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Project2UITestsVersionString[];
+
